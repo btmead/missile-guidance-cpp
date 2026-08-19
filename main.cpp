9183ed3dabd9d8d127e2aa7b7471783693d99e38
Ben Mead <benmead3@gmail.com>
2026-08-19 18:49:32 +0100

    Initialisation of project

---
+++ main.cpp
@@ -0,0 +1,14 @@
+//
+// Created by benme on 19/08/2026.
+//
+
+#include "main.h"
+#include <iostream>
+
+
+int main() {
+
+
+    return 0;
+}
+
---
+++ main.h
@@ -0,0 +1,14 @@
+//
+// Created by benme on 19/08/2026.
+//
+
+#ifndef GUIDANCE_SIM_CPP_MAIN_H
+#define GUIDANCE_SIM_CPP_MAIN_H
+
+
+class main
+{
+};
+
+
+#endif //GUIDANCE_SIM_CPP_MAIN_H
